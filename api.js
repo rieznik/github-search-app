568af23026997f0519ca5187dc4a0a3dbe5a5a12
Kateryna Rieznik <rieznik.k@gmail.com>
2023-05-14 12:09:11 +0300

    remove secrets

--- api.js
+++ api.js
@@ -1,6 +1,3 @@
-const CLIENT_ID = "6ea01c207144ba539c8f";
-const CLIENT_SECRET = "0498c5931d5620e4319f9f71b93a45b5886ca8bb";
-
 export class API {
   #reposNumber = 5;
   async getUserData(input) {
@@ -8,7 +5,7 @@ export class API {
       throw new Error("Please enter a username");
     }
     const response = await fetch(
-      `https://api.github.com/users/${input}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      `https://api.github.com/users/${input}`
     );
     const data = await response.json();
     if (data.message === "Not Found") {
@@ -21,7 +18,7 @@ export class API {
     const response = await fetch(
       `https://api.github.com/users/${input}/repos?per_page=${
         this.#reposNumber
-      }&sort=updated&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+      }&sort=updated`
     );
     const data = await response.json();
     return data;
